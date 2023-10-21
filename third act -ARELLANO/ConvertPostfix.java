d73233771362fc13746ececd08968f88fb0ee170
rustumsk <rustumarellano@gmail.com>
2023-10-21 12:47:08 +0800

    it can now read multi digit

--- third act -ARELLANO/ConvertPostfix.java
+++ third act -ARELLANO/ConvertPostfix.java
@@ -1,20 +1,21 @@
 import java.util.Stack;
 import java.util.Scanner;
+
 public class ConvertPostfix {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter infix to convert to postfix: ");
+        System.out.print("Enter infix to convert to postfix(without spaces! example: (1+2-3)): ");
         String infix = scan.nextLine();
-        Convert convert = new Convert(infix);   
+        Convert convert = new Convert(infix);
 
         String result = convert.convertPostfix();
 
         System.out.println(result);
 
         scan.close();
-        
     }
 }
+
 class Convert {
     private StringBuilder nResult = new StringBuilder();
     private Stack<Character> operator = new Stack<>();
@@ -29,20 +30,26 @@ class Convert {
             char c = passed.charAt(i);
 
             if (Character.isDigit(c)) {
-                nResult.append(c); // Append operands directly to nResult
-            } else if (c == '(') {
+                StringBuilder multi = new StringBuilder();
+                while (i < passed.length() && Character.isDigit(passed.charAt(i))) { // this adds and appends the character whether it is multi digit or single.
+                    multi.append(passed.charAt(i));
+                    i++;// increment so the count will iterate correctly.
+                }
+                nResult.append(multi).append(" ");;
+                i--; // Decrement i to revisit the operator character
+            } else if (c == '(') { //check if there is a parenthesis
                 operator.push(c);
             } else if (c == ')') {
-                while (operator.peek() != '(') {
-                    nResult.append(operator.pop()); // Append operators to nResult
+                while (operator.peek() != '(') { //if it encounters a closing parenthisis it appends all
+                    nResult.append(operator.pop()).append(" ");
                 }
-                operator.pop(); // Pop the opening parenthesis
+                operator.pop();
             } else {
-                if (operator.isEmpty()) {
+                if (operator.isEmpty() || operator.peek() == '(') {
                     operator.push(c);
                 } else {
                     while (!operator.isEmpty() && checkPrecedence(c) <= checkPrecedence(operator.peek())) {
-                        nResult.append(operator.pop()); // Append operators to nResult
+                        nResult.append(operator.pop()).append(" ");
                     }
                     operator.push(c);
                 }
@@ -50,7 +57,7 @@ class Convert {
         }
 
         while (!operator.isEmpty()) {
-            nResult.append(operator.pop()); // Append remaining operators to nResult
+            nResult.append(operator.pop()).append(" ");
         }
 
         return nResult.toString();
